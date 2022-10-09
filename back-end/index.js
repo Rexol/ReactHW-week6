03b3051e8e8f6e60b65ea533faebad999629a28c
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2022-10-09 16:41:44 +0300

    final commit

---
+++ back-end/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+const config = require('./config');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const port = 3001;
+
+
+app.get('/', async function (req, res) {
+    try {
+        console.log('connecting');
+        const connection = await mysql.createConnection(config.db);
+        const [result,] = await connection.execute('select * from task');
+        if (!result) result = [];
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post('/new', async function(req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db);
+        const [result,] = await connection.execute('insert into task(description) values (?)', [req.body.description]);
+        res.status(200).json({id:result.insertId});
+    } catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+app.delete('/delete/:id', async function(req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db);
+        const [result,] = await connection.execute('delete from task where id = (?)', [req.params.id]);
+        res.status(200).json({id:req.params.id});
+    } catch(err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+app.listen(port,  () => {
+  console.log(`Server app listening on port ${port}`)
+});
